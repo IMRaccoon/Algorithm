d96151593413a7c0279d1a2be797a4fd8347bdbb
DoHyeon Jeon <whdies816@gmail.com>
2021-07-14 01:53:04 +0900

    Baekjoon Problem 2018 Clear

--- Java/baekjoon/Problem_2108.java
+++ Java/baekjoon/Problem_2108.java
@@ -1,40 +1,73 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.HashMap;
 
 public class Problem_2108 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        int [] arr = new int[8001];
-        float sum = 0;
-        for(int i = 0 ; i < n; i++) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int frequency = -1;
+        int frequencyCounter = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, tmp;
+
+        for (int i = 0; i < n; i++) {
             int d = Integer.parseInt(br.readLine());
-            arr[d + 4000]++;
+            min = Math.min(min, d);
+            max = Math.max(max, d);
+            if (map.containsKey(d)) {
+                tmp = map.get(d) + 1;
+                map.replace(d, tmp);
+            } else {
+                tmp = 1;
+                map.put(d, 1);
+            }
+
+            if (frequency < tmp) {
+                frequency = tmp;
+                frequencyCounter = 1;
+            } else if (frequency == tmp) {
+                frequencyCounter += 1;
+            }
+
             sum += d;
         }
-        System.out.println(Math.round(sum / (float)n));
+        bw.write(Math.round((double) sum / (double) n) + "\n");
 
-        int mid = 0, min = 0, max = 0;
-        int index = 0;
-        for(int i = 0 ; i <= 8000; i++) {
-            if(arr[i] != 0) {
-                if(index == 0) {
-                    min = arr[i] - 4000;
-                }
-                index += arr[i];
+        int mid = Math.floorDiv(n, 2) + 1;
+        int minFrequency = -4001;
+        int count = 0;
+        boolean midFlag = true, frequencyFlag = true;
 
-                if(mid == 0 && (index + 1) * 2 >= n) {
-                    mid = arr[i] - 4000;
-                }
-                if(index == n) {
-                    max = arr[i] - 4000;
+        int[] keys = map.keySet().stream().sorted().mapToInt(Integer::intValue).toArray();
+
+        for (int k : keys) {
+            tmp = map.get(k);
+            if (midFlag) {
+                count += tmp;
+                if (count >= mid) {
+                    bw.write(k + "\n");
+                    midFlag = false;
                 }
             }
+            if (frequencyFlag && (tmp == frequency)) {
+                if (frequencyCounter == 1) {
+                    minFrequency = k;
+                    frequencyFlag = false;
+                } else {
+                    frequencyCounter = 1;
+                }
+            }
+
+            if (!frequencyFlag && !midFlag) {
+                break;
+            }
         }
-        System.out.println(mid);
-        System.out.println(min);
-        System.out.println(max - min);
+
+        bw.write(minFrequency + "\n");
+        bw.write(max - min + "\n");
+        bw.flush();
     }
 }

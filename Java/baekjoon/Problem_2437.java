b6a57db9c1de97685661a86f6f05afa3f0578f4f
DoHyeon Jeon <whdies816@gmail.com>
2021-07-20 14:24:40 +0900

    Baekjoon Problem 2437 Clear

---
+++ Java/baekjoon/Problem_2437.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem_2437 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        int answer = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            if (answer + 1 >= arr[i]) {
+                answer += arr[i];
+            } else {
+                break;
+            }
+        }
+        System.out.println(answer + 1);
+    }
+}

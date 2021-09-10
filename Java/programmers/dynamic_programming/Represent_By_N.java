e4f4a2c40864c2146be36ed117983fe19a15b95d
DoHyeon Jeon <whdies816@gmail.com>
2021-09-10 15:47:15 +0900

    Programmers N으로 표현 Clear

--- Java/programmers/dynamic_programming/Represent_By_N.java
+++ Java/programmers/dynamic_programming/Represent_By_N.java
@@ -1,10 +1,8 @@
 package dynamic_programming;
-/*
-        N은 1 이상 9 이하입니다.
-        number는 1 이상 32,000 이하입니다.
-        수식에는 괄호와 사칙연산만 가능하며 나누기 연산에서 나머지는 무시합니다.
-        최솟값이 8보다 크면 -1을 return 합니다.
-*/
+
+
+import java.util.ArrayList;
+
 public class Represent_By_N {
     public static void main(String[] args) {
         Represent_By_N RN = new Represent_By_N();
@@ -13,9 +11,55 @@ public class Represent_By_N {
         System.out.println(solution(5, 12));
         System.out.println(solution(2, 11));
     }
-    public int solution(int N, int number) {
-        int answer = 8;
 
-        return answer;
+    int MAX = 1_000_000_000;
+    public int solution(int N, int number) {
+        boolean [] visited = new boolean[MAX];
+        int acc = 0;
+        ArrayList<Integer>[] list = new ArrayList[9];
+        for(int i = 1; i <= 8; i++) {
+            acc = (acc * 10) + N;
+            list[i] = new ArrayList<>();
+            list[i].add(acc);
+            if(acc == number) return i;
+            visited[acc] = true;
+        }
+
+        for(int i = 2; i <= 8; i++) {
+            for (int j = 1; j < i; j++) {
+                for (int first : list[j]) {
+                    for (int second : list[i - j]) {
+                        int res = first + second;
+                        if (!visited[res]) {
+                            if(res == number) return i;
+                            visited[res] = true;
+                            list[i].add(res);
+                        }
+
+                        res = first - second;
+                        if (res > 0 && !visited[res]) {
+                            if(res == number) return i;
+                            visited[res] = true;
+                            list[i].add(res);
+                        }
+
+                        res = first * second;
+                        if (!visited[res]) {
+                            if(res == number) return i;
+                            visited[res] = true;
+                            list[i].add(res);
+                        }
+
+                        res = Math.floorDiv(first, second);
+                        if (res > 0 && !visited[res]) {
+                            if(res == number) return i;
+                            visited[res] = true;
+                            list[i].add(res);
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
     }
 }
